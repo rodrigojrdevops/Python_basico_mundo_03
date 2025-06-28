5814d920520c94ccf815f3e8ade8c57544911409
Rodrigo dos Santos <rodrigojrdev1@gmail.com>
2025-06-28 18:39:52 -0300

    Update exerc_02.py

--- Python_part_00/exerc_02.py
+++ Python_part_00/exerc_02.py
@@ -14,4 +14,13 @@ times = ('Corinthias', 'Palmeiras', 'Santos', 'Grêmio',
          'Atlético-GO')
 print('-=' * 15)
 print(f'Lista de times do Brasileirão: {times}')
+print('-=' * 15)
+print(f'Os 5 primeiros são {times[0:5]}')
+print('-=' * 15)
+print(f'Os 4 últimos que estão na zona de rebaixamento são {times[-4:]}')
+print('-= ' * 15)
+print(f'Times em ordem alfabética: {sorted(times)}')
+print('-= ' * 15)
+print(f'A chapecoense está na {times.index("Chapecoense")+1}ª')
+
 
