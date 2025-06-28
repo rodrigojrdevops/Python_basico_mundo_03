7d71d4d68498630f48d60dcdb0d4f513512a5d6c
Rodrigo dos Santos <rodrigojrdev1@gmail.com>
2025-06-28 11:46:29 -0300

    Create exerc_01.py

---
+++ Python_part_00/exerc_01.py
@@ -0,0 +1,3 @@
+#Crie um programa que tenha um tupla totalmente preenchida com uma contagem por extenso, de ero até vinte.
+#Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
+
