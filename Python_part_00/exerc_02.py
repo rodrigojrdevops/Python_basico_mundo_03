5212eb13923a8f31ba29a1afc4c36f3dae890773
Rodrigo dos Santos <rodrigojrdev1@gmail.com>
2025-06-28 13:55:37 -0300

    Create exerc_02.py

---
+++ Python_part_00/exerc_02.py
@@ -0,0 +1,17 @@
+#Fatiamento e acesso a tuplas
+
+#Crie uma tupla preechida com os 20 primeiros colocados da tabela do Campeonato Brasileiro de futebol, na ordem de colocação.Depois mostre:
+#Os 5 primeiros.
+#Os últimos 4 colocados
+#Times em ordem alfabética.
+#Em que posição está o time da Chapecoense.
+
+times = ('Corinthias', 'Palmeiras', 'Santos', 'Grêmio', 
+         'Cruzeiro', 'Flamengo', ' Vasco', 'Chapecoense', 
+         'Atlético', 'Botafogo', 'Atlético-PR', 'Bahia',
+         'São Paulo', 'Fluminense', 'Sport Recife',
+         'EC Vitória', 'Coritiba', 'Avaí', 'Ponte Preta',
+         'Atlético-GO')
+print('-=' * 15)
+print(f'Lista de times do Brasileirão: {times}')
+
