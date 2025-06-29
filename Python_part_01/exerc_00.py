76b29e8cb693af9dc352186b87ddebe4042ea143
Rodrigo dos Santos <rodrigojrdev1@gmail.com>
2025-06-29 15:12:59 -0300

    Create exerc_00.py

---
+++ Python_part_01/exerc_00.py
@@ -0,0 +1,3 @@
+#lanche.insert(0,'elemento') // adiciona elemento no inicio da lista onde você fatiar
+#lanche.append () // adiciona um elemento no final da lista
+#del.lanche [3] // lanche.pop[3]// lanche.remove('Elemento') // elimina um item
